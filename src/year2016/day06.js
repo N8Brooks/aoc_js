46875109d640b954bca92fca1cc06c1970764ce5
Nathan Brooks <natebrooks004@gmail.com>
2021-01-13 13:29:39 -0800

    make private

--- src/year2016/day06.js
+++ src/year2016/day06.js
@@ -4,17 +4,19 @@ import _ from "lodash";
 import { readFileSync } from "fs";
 
 class Counter {
+  #counts;
+
   constructor(elements) {
-    this.counts = new Map();
+    this.#counts = new Map();
     for (const key of elements) {
-      this.counts.set(key, this.counts.get(key) + 1 || 1);
+      this.#counts.set(key, this.#counts.get(key) + 1 || 1);
     }
   }
 
   mode() {
     let maxKey,
       maxVal = -Infinity;
-    for (const [key, val] of this.counts) {
+    for (const [key, val] of this.#counts) {
       if (maxVal < val) {
         maxKey = key;
         maxVal = val;
@@ -26,7 +28,7 @@ class Counter {
   antimode() {
     let minKey,
       minVal = Infinity;
-    for (const [key, val] of this.counts) {
+    for (const [key, val] of this.#counts) {
       if (val < minVal) {
         minKey = key;
         minVal = val;
