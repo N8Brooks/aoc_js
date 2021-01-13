9bd20e6be0be097629c9e34ec74b7931252c9098
Nathan Brooks <natebrooks004@gmail.com>
2021-01-12 22:09:54 -0800

    eslint

--- aoc/year2016/day03.js
+++ aoc/year2016/day03.js
@@ -1,30 +1,28 @@
-import {zip, chunk} from "lodash";
-import {readFileSync} from "fs";
+import _ from "lodash";
+import { readFileSync } from "fs";
 
 function process(text) {
-    return text
-        .trim()
-        .split("\n")
-        .map((line) => line.match(/\d+/g).map(Number));
+  return text
+    .trim()
+    .split("\n")
+    .map((line) => line.match(/\d+/g).map(Number));
 }
 
 function total(sum, [a, b, c]) {
-    return sum + (a < b + c && b < a + c && c < a + b);
+  return sum + (a < b + c && b < a + c && c < a + b);
 }
 
 export function part1(text) {
-    return process(text).reduce(total, 0);
+  return process(text).reduce(total, 0);
 }
 
 export function part2(text) {
-    return zip(...process(text))
-        .map((col) => chunk(col, 3).reduce(total, 0))
-        .reduce((a, b) => a + b);
+  return _.zip(...process(text))
+    .map((col) => _.chunk(col, 3).reduce(total, 0))
+    .reduce((a, b) => a + b);
 }
 
-if (require.main === module) {
-    const text = readFileSync("../../data/year2016/day03.txt", "utf8");
+const text = readFileSync("data/year2016/day03.txt", "utf8");
 
-    console.log(part1(text));
-    console.log(part2(text));
-}
+console.log(part1(text));
+console.log(part2(text));
---
+++ test/year2016/test_day03.js
@@ -0,0 +1,42 @@
+import chai from "chai";
+import { readFileSync } from "fs";
+
+import { part1, part2 } from "../../aoc/year2016/day03.js";
+
+const text = readFileSync("data/year2016/day03.txt", "utf8");
+
+const example = `101 301 501
+102 302 502s
+103 303 503
+201 401 601
+202 402 602
+203 403 603
+`;
+
+describe("part1", () => {
+  it("input", () => {
+    chai.expect(part1(text)).to.equal(869);
+  });
+
+  it("example 1", () => {
+    chai.expect(part1("5 10 25")).to.equal(0);
+  });
+
+  it("example 2", () => {
+    chai.expect(part1(example)).to.equal(3);
+  });
+});
+
+describe("part2", () => {
+  it("input", () => {
+    chai.expect(part2(text)).to.equal(1544);
+  });
+
+  it("example 1", () => {
+    chai.expect(part2("5 10 25")).to.equal(0);
+  });
+
+  it("example 2", () => {
+    chai.expect(part2(example)).to.equal(6);
+  });
+});
