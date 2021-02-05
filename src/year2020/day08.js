336a23b0e1a0792703837f1589dc22ea42f62e62
Nathan Brooks <natebrooks004@gmail.com>
2021-02-04 21:57:01 -0800

    2020/08

---
+++ src/year2020/day08.js
@@ -0,0 +1,96 @@
+export function part1(text) {
+  const instruction = process(text);
+  const length = instruction.length;
+  const seen = new Set();
+  let cumulative = 0;
+  let i = 0;
+
+  while (i < length) {
+    if (seen.has(i)) {
+      return cumulative;
+    }
+
+    seen.add(i);
+    const [op, num] = instruction[i];
+
+    if (op === "acc") {
+      cumulative += num;
+      i++;
+    } else if (op === "jmp") {
+      i += num;
+      continue;
+    } else {
+      i++;
+    }
+  }
+}
+
+export function part2(text) {
+  const instruction = process(text);
+  const length = instruction.length;
+  const seen = new Set();
+  let cumulative = 0;
+  let swapped = false;
+  let result;
+
+  return parse(0), cumulative;
+
+  function parse(i) {
+    if (seen.has(i)) {
+      return;
+    }
+
+    if (i === length) {
+      return true;
+    }
+
+    seen.add(i);
+    const [op, num] = instruction[i];
+    result = op === "acc" ? acc(i, num) : op === "jmp" ? jmp(i, num) : nop(i, num);
+    seen.delete(i);
+
+    return result;
+  }
+
+  function acc(i, num) {
+    cumulative += num;
+    if (parse(i + 1)) {
+      return true;
+    }
+    cumulative -= num;
+  }
+
+  function jmp(i, num) {
+    if (parse(i + num)) {
+      return true;
+    }
+    if (!swapped) {
+      swapped = true;
+      if (parse(i + 1)) {
+        return true;
+      }
+      swapped = false;
+    }
+  }
+
+  function nop(i, num) {
+    if (parse(i + 1)) {
+      return true;
+    }
+    if (!swapped) {
+      swapped = true;
+      if (parse(i + num)) {
+        return true;
+      }
+      swapped = false;
+    }
+  }
+}
+
+function process(text) {
+  return text
+    .trim()
+    .split("\n")
+    .map((line) => line.split(" "))
+    .map(([op, num]) => [op, +num]);
+}
---
+++ tst/year2020/test_day08.js
@@ -0,0 +1,38 @@
+import chai from "chai";
+import { readFileSync } from "fs";
+import { part1, part2 } from "../../src/year2020/day08.js";
+
+const text = readFileSync("data/year2020/day08.txt", "utf8");
+
+const example = `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+`;
+
+describe("day08", () => {
+  describe("part1", () => {
+    it("input", () => {
+      chai.expect(part1(text)).to.equal(1766);
+    });
+
+    it("example", () => {
+      chai.expect(part1(example)).to.equal(5);
+    });
+  });
+
+  describe("part2", () => {
+    it("input", () => {
+      chai.expect(part2(text)).to.equal(1639);
+    });
+
+    it("example", () => {
+      chai.expect(part2(example)).to.equal(8);
+    });
+  });
+});
