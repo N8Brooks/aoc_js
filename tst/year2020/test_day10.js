99fa337fc605c89cb9d7d3468ff5978b6ade010c
Nathan Brooks <natebrooks004@gmail.com>
2021-02-06 13:32:12 -0800

    2020/10

--- tst/year2020/test_day10.js
+++ tst/year2020/test_day10.js
@@ -4,53 +4,12 @@ import { part1, part2 } from "../../src/year2020/day10.js";
 
 const text = readFileSync("data/year2020/day10.txt", "utf8");
 
-const example_1 = `16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4
-`;
+const example_1 = `16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4\n`;
 
-const example_2 = `28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3
-`;
+const example_2 = `28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19
+38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3\n`;
 
-describe("day09", () => {
+describe("day10", () => {
   describe("part1", () => {
     it("input", () => {
       chai.expect(part1(text)).to.equal(2760);
