52fc7920f1f9ba58f2dc8c2eec67cb0300bbbc60
Nathan Brooks <natebrooks004@gmail.com>
2021-02-07 19:59:37 -0800

    2020/17

---
+++ src/year2020/day17.js
@@ -0,0 +1,49 @@
+import { CartesianProduct } from "js-combinatorics/commonjs/combinatorics.js";
+import _ from "lodash";
+
+export function part1(text, dimensions = 3) {
+  return process(text, dimensions);
+}
+
+export function part2(text, dimensions = 4) {
+  return process(text, dimensions);
+}
+
+function process(text, dimensions, range = 6) {
+  const pad = Array(dimensions - 2).fill(0);
+  const directions = Array.from(new CartesianProduct(...Array(dimensions).fill([-1, 1, 0])));
+  directions.pop();
+
+  const parse = _.memoize((coord) => JSON.parse(`[${coord}]`));
+
+  let coords = {};
+  text
+    .trim()
+    .split("\n")
+    .forEach((row, y) => {
+      row
+        .trim()
+        .split("")
+        .map((cell, x) => [cell, x])
+        .filter(([cell]) => cell === "#")
+        .forEach(([, x]) => (coords[[x, y, ...pad]] = undefined));
+    });
+
+  for (let cycle = 0; cycle < range; cycle++) {
+    const counts = {};
+    Object.keys(coords).forEach((coord) => {
+      directions.forEach((dir) => {
+        const neighbor = parse(coord).map((x, i) => x + dir[i]);
+        counts[neighbor] = (counts[neighbor] ?? 0) + 1;
+      });
+    });
+
+    coords = Object.fromEntries(
+      Object.entries(counts)
+        .filter(([coord, count]) => count === 3 || (count === 2 && coord in coords))
+        .map(([coord]) => [parse(coord), undefined])
+    );
+  }
+
+  return Object.keys(coords).length;
+}
---
+++ tst/year2020/test_day17.js
@@ -0,0 +1,32 @@
+import chai from "chai";
+import { readFileSync } from "fs";
+import { part1, part2 } from "../../src/year2020/day17.js";
+
+const text = readFileSync("data/year2020/day17.txt", "utf8");
+
+const example = `.#.
+..#
+###
+`;
+
+describe("day17", () => {
+  describe("part1", () => {
+    it("input", () => {
+      chai.expect(part1(text)).to.equal(368);
+    });
+
+    it("example", () => {
+      chai.expect(part1(example)).to.equal(112);
+    });
+  });
+
+  describe("part2", () => {
+    it.skip("input", () => {
+      chai.expect(part2(text)).to.equal(2696);
+    });
+
+    it("example", () => {
+      chai.expect(part2(example)).to.equal(848);
+    });
+  });
+});
